c9d7ee91a15b82164cb20331c6a98402c85d2129
Eli <eli.quinio17@gmail.com>
2025-04-30 03:07:38 +0800

    Add MyApp Class to main for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:bir_pos/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // For dev env variables
 
-void main() {
-  runApp(MaterialApp(home: Login()));
+void main() async {
+  await dotenv.load(fileName: ".env"); // Load .env file
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Login());
+  }
 }
