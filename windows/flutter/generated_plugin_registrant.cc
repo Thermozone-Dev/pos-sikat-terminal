c314b31063073e9f840109cea0e6594f610040f8
Eli <eli.quinio17@gmail.com>
2025-05-01 18:17:10 +0800

    Update plugins on build

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_pos_printer_platform_image_3/flutter_pos_printer_platform_plugin.h>
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterPosPrinterPlatformPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterPosPrinterPlatformPlugin"));
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
 }
