9ba839fe11cbdf87e194c8bae6394587674051d7
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-07-17 01:14:57 +0800

    Fix by Manual Adding of Certificate

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
-import 'package:bir_pos/login.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // For dev env variables
 import 'dart:io';
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
+import 'package:bir_pos/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // For dev env variables
 
 void main() async {
-  await dotenv.load(fileName: ".env"); // Load .env file
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ByteData data = await rootBundle.load('assets/ca/lets-encrypt-r3.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(
+    data.buffer.asUint8List(),
+  );
+
   runApp(MyApp());
 }
 
