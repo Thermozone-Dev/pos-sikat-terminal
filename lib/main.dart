f50498d010f487a7be2832bd6b94115457a215c7
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-07-17 00:35:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:bir_pos/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart'; // For dev env variables
+import 'dart:io';
 
 void main() async {
   await dotenv.load(fileName: ".env"); // Load .env file
   runApp(MyApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class MyApp extends StatelessWidget {
